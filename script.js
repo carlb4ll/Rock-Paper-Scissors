2b7822a699e34fd57cd240cfe30ef0fb43270de6
carlb4ll <carlball28@gmail.com>
2022-06-17 11:41:28 +0100

    requirements added to script

---
+++ script.js
@@ -0,0 +1,10 @@
+// Rock Paper Scissors Console Game
+
+// Requirements
+
+//Rock crushes scissors, scissors cuts paper, and paper covers rock.
+
+//Functions - computerPlay - computer choosing a random string Rock, Paper or Scissors returna value called computer selection
+//User input case-insensitive string that reurns player selection
+//playRound plays a single round
+//game play 5 rounds
