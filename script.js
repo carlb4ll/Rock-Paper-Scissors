b69703579f20bcca0b3874d4fc5c4268adb459d4
carlb4ll <carlball28@gmail.com>
2022-06-17 12:27:08 +0100

    basic game mechanics working

--- script.js
+++ script.js
@@ -8,3 +8,49 @@
 //User input case-insensitive string that reurns player selection
 //playRound plays a single round
 //game play 5 rounds
+
+const gameOptions = ["rock", "paper", "scissors"];
+
+const gameResults = [
+  "it's a tie, you both choose the same",
+  "Rock crushes scissors, you lose!",
+  "Rock crushes scissors, you win!",
+  "Scissors cuts paper, you lose!",
+  "Scissors cuts paper, you win!",
+  "Paper covers rock, you lose!",
+  "Paper covers rock, you win!",
+];
+
+let computerSelection =
+  gameOptions[Math.floor(Math.random() * gameOptions.length)];
+let userInput = prompt("Choose Rock, Paper or Scissors?");
+let playerSelection = userInput.toLowerCase();
+let roundResult;
+
+function playRound(computerSelection, playerSelection) {
+  if (computerSelection === playerSelection) {
+    roundResult = 0;
+  } else if (
+    computerSelection === gameOptions[0] &&
+    playerSelection === gameOptions[2]
+  ) {
+    roundResult = 1;
+  } else if (
+    computerSelection === gameOptions[2] &&
+    playerSelection === gameOptions[1]
+  ) {
+    roundResult = 3;
+  } else {
+    console.log("just testing");
+  }
+  console.log(gameResults[roundResult]);
+}
+
+playRound();
+
+// Rock crushes scissors, you lose!
+// Rock crushes scissors, you win!
+// Scissors cuts paper, you lose!
+// Scissors cuts paper, you win!
+// Paper covers rock, you lose!
+// Paper covers rock, you win!
